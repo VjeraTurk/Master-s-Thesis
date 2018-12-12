479f8be82bf47339b74ac316c6729144f2a9ed57
VjeraTurk <vjera.turk@gmail.com>
2018-12-12 17:04:59 +0100

    Trouble installing rgdalgit status!

---
+++ source/voronoi.r
@@ -0,0 +1,41 @@
+pkgs <- c("sp","rgdal","deldir","dplyr","ggthemes")
+install.packages(pkgs)
+install.packages("rgdal")
+
+require(sp)
+require(rgdal)
+require(deldir)
+require(dplyr)
+require(ggplot2)
+require(ggthemes)
+
+SPointsDF_to_voronoi_SPolysDF <- function(sp) {
+  
+  # tile.list extracts the polygon data from the deldir computation
+  vor_desc <- tile.list(deldir(sp@coords[,1], sp@coords[,2]))
+  
+  lapply(1:(length(vor_desc)), function(i) {
+    
+    # tile.list gets us the points for the polygons but we
+    # still have to close them, hence the need for the rbind
+    tmp <- cbind(vor_desc[[i]]$x, vor_desc[[i]]$y)
+    tmp <- rbind(tmp, tmp[1,])
+    
+    # now we can make the Polygon(s)
+    Polygons(list(Polygon(tmp)), ID=i)
+    
+  }) -> vor_polygons
+  
+  # hopefully the caller passed in good metadata!
+  sp_dat <- sp@data
+  
+  # this way the IDs _should_ match up w/the data & voronoi polys
+  rownames(sp_dat) <- sapply(slot(SpatialPolygons(vor_polygons),
+                                  'polygons'),
+                             slot, 'ID')
+  
+  SpatialPolygonsDataFrame(SpatialPolygons(vor_polygons),
+                           data=sp_dat)
+  
+}
+

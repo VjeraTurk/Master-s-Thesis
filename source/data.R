1e878dcc07e4869719924ec7d5b45601d4ff9714
VjeraTurk <vjera.turk@gmail.com>
2019-01-08 17:58:36 +0100

    random sampling not in order

--- source/data.R
+++ source/data.R
@@ -46,12 +46,20 @@ file = paste(getwd(),"/PhoneData_ordered_by_Time.csv",sep="")
 phone = c("SIM Card ID", "Time", "Latitude", "Longitude")
 system.time( PhoneData <- fread(file=file, sep="auto", header=FALSE, col.names = phone, quote="")) # bitno da ima quote 
 
+
 #PDsample = head(PhoneData,50000)
-system.time( PDsample -> PhoneData[sample(nrow(PhoneData), 50000), ])#random smaple https://stat.ethz.ch/pipermail/r-help/2007-February/125860.html,  zdere ram, blocka se ako nema dosta
+system.time( PDsample -> PhoneData[sample(nrow(PhoneData), 50000), ])
+#random smaple https://stat.ethz.ch/pipermail/r-help/2007-February/125860.html,  zdere ram, blocka se ako nema dosta
+#TODO: izgubi se ORDER!
+system.time(setorder(PDsample, Time))
+
+file = paste(getwd(),"/PDsample_50k.RData",sep="")
+load(file = file)
+
+PDsample = PDsample_50k
 
 PDsample_backup = copy(PDsample) # copy, inace pointer
 PDsample = copy(PDsample_backup)
-
 
 #require(hsm)
 #PDsample$Time = as.hsm(PDsample$Time)
@@ -62,30 +70,58 @@ PDsample = copy(PDsample_backup)
 #PDsample[, posixct:= as.POSIXct(paste(Time),format("%T"),tz="GTM")]
 # POSIX jednostavno nema format, klasa je datetime i tjt.
 
-#t = strptime(PDsample$Time,"%T") #doda datum
+t = strptime(PDsample$Time,"%T") #doda datum
+#t = parse_date_time(PDsample$Time, "H%:M%:S%") #doda datum, ali ne danasnji
 
 
 require(lubridate)
 require(chron)
 
-#onlytime <- times(t)
-onlytime = lubridate::hms(PDsample$Time)
-PDsample[, .(count = .N), by = .(Time, interval = hour(onlytime) %/% 3)]
+onlytime <- times(t)
+system.time(onlytime <- lubridate::hms(PDsample$Time))
+# 50 k    
+# user  system elapsed 
+#0.036   0.000   0.068 
 
-onlytime = lubridate::hms(PhoneData$Time)
+PDsample[, .(count = .N), by = .(Time, interval = hour(onlytime) %/% 3)] #GOOD # rijesenje su stupci Time interval i count, no nisu pridodani kao dio PDsample
+
+#traje dugo (ne dovrsava se):
+onlytime = lubridate::hms(PhoneData$Time) 
+
 PhoneData[, .(count = .N), by = .(Time, interval = hour(onlytime) %/% 3)]
 
 
+"""
+Analysis of large groups of time series
+Time series clustering is implemented in TSclust, dtwclust, BNPTSclust and pdc.
 
-t=parse_date_time(PDsample$Time, "H%:M%:S%") #doda datum, ali ne danasnji
+TSdist provides distance measures for time series data.
 
-PDsample[, .(count = .N), by = .(Time, interval = hour(Time) %/% 3)]
+TSrepr includes methods for representing time series using dimension reduction and feature extraction.
+
+jmotif implements tools based on time series symbolic discretization for finding motifs in time series 
+and facilitates interpretable time series classification.
+
+rucrdtw provides R bindings for functions from the UCR Suite to enable ultrafast subsequence search for 
+a best match under Dynamic Time Warping and Euclidean Distance.
+
+Methods for plotting and forecasting collections of hierarchical and grouped time series are provided by
+hts. thief uses hierarchical methods to reconcile forecasts of temporally aggregated time series. 
+An alternative approach to reconciling forecasts of hierarchical time series is provided by gtop. thief
+"""
+
+
+require(timeSeries)#rovides a class and various tools for financial time series. This includes basic functions such as scaling and sorting, subsetting, mathematical operations and statistical functions.
+require(jmotif)#jmotif implements tools based on time series symbolic discretization for finding motifs in time series and facilitates interpretable time series classification.
+require(xts)
+PDsample$Time
+
+
+
 #split(PDsample, cut(PDsample$Time,breaks = "hour"))
 ##PDsample$group = cumsum(ifelse(difftime(PDsample$Time, shift( PDsample$Time, fill = PDsample$Time[1]),units = "hours") >=3))
 
 require(h2o)
-
-
 ########################################
 
 file = paste(getwd(),"/TaxiData",sep="")

c7cbe7d6b6aa999c36a8dbf2a7b20c7847b6fcd1
VjeraTurk <vjera.turk@gmail.com>
2019-02-25 17:58:23 +0100

    moved code to stop_sequence_generation to combine functions and get less execution time

--- source/journey_generation.r
+++ source/journey_generation.r
@@ -1,3 +1,18 @@
+require("optimbase")
+
+file = paste(getwd(),"/valid_stop_df_1m.RData", sep="")
+system.time(load(file = file))
+
+
+file = paste(getwd(),"/LatLon.RData", sep="")
+system.time(load(file = file))
+system.time(Lat_Lon <- paste(LatLon$Latitude,LatLon$Longitude, sep= "_"))
+
+OD_0_24_SH<<-zeros(length(Lat_Lon),length(Lat_Lon))
+#names(OD_0_24_SH)<- Lat_Lon# names je krivo, treba dimnames()
+#http://www.r-tutor.com/r-introduction/matrix
+
+dimnames(OD_0_24_SH)<-list(Lat_Lon,Lat_Lon)
 "
 Journey Identification: Stop sequences were then converted into a set of journeys for each subscriber.
 A journey is defined as 2 contiguous 'stops' that are separated by a period of at least t_min, but no more
@@ -7,6 +22,9 @@ a journey that, in actually, is missing a midpoint destination.
 "
 #Journey Identification
 to_keep_df<<-data.frame()
+#journeys<<-data.frame()
+#c("Start_Time","Stop_Time","Origin","Destinaton")
+
 journey_identification<-function(dataframe,t_min, t_max){
   
   for(i in 2:nrow(dataframe)){
@@ -20,6 +38,12 @@ journey_identification<-function(dataframe,t_min, t_max){
       #stop ce dodati 2 puta, jednom kao destination, jednom kao origin
       to_keep_df<<- rbind(to_keep_df, prev) 
       to_keep_df<<- rbind(to_keep_df, curr)
+      
+      orig = paste(prev$Latitude,prev$Longitude,sep = "_")
+      dest = paste(curr$Latitude,curr$Longitude,sep = "_")
+      
+      OD_0_24_SH[origin,dest] = OD_0_24_SH[orig,dest] + 1
+      
     }
   }
 }
@@ -30,9 +54,16 @@ t_max = difftime(parse_date_time("04:00:00", "H%:M%:S%"), parse_date_time("00:00
 # sugesting 198 000 journeys in a day from 36 000 000 events
 # which makes average of 24750 journeys per 3 hour period
 
-
+df<-valid_stop_df
 system.time(sapply(unique(df$ID), function (value) journey_identification(df[df$ID == value, ],t_min,t_max),simplify = FALSE))
+#user  system elapsed 
+#16.196   0.000  16.213
+# 9086 obs.
+
 journey_set<-to_keep_df
+journey_set_1m<-journey_set
+
+save(journey_set_1m, file = paste(getwd(),"/journey_set_1m.RData", sep=""))
 
 #### Confidence Tresholding
 #### Journey Cleansing
@@ -44,5 +75,5 @@ minimum journey duration, minimum journey distance or journey purpose (based on 
 other), etc. However, at this point journey counts are restricted to our sample size.
 "
 ### Matrix Generation
-
+system.time(setorder(journey_set,Latitude,Longitude))
 

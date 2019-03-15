7ee5685f0124244caba3b4375435bc33911b8e0b
VjeraTurk <vjera.turk@gmail.com>
2019-03-15 01:20:33 +0100

    Shenzhen osm bbox file exported using qgis
    Former-commit-id: 933302d3c55b8173539ce9bb58a64f9a75ed6838

--- source/osm.R
+++ source/osm.R
@@ -1,3 +1,7 @@
 setwd("~/CODM/masters-thesis/data/osm")
 require("osmar")
-system.time(get_osm(complete_file(), source = osmsource_file("~/CODM/masters-thesis/data/osm/shenzhen.osm")))
+system.time(map<-get_osm(complete_file(), source = osmsource_file("~/CODM/masters-thesis/data/osm/shenzhen.osm")))
+#user  system elapsed 
+#571.016   1.100 573.208 
+
+save(map, file = paste(getwd(),"/map.RData",sep=""))

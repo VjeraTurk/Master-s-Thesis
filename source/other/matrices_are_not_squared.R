0d7d5f832f848d8034549e1fbac8256c0a4011ea
VjeraTurk <vjera.turk@gmail.com>
2019-05-29 23:49:09 +0200

    SSIM, MSE and R^2 on 1m records cdr and taxi ODMs, SSIM is around 0.83
    Former-commit-id: 60ea55f4bb43caf6a4795520f65e2ee8114a1e95

--- source/other/matrices_are_not_squared.R
+++ source/other/matrices_are_not_squared.R
@@ -1,22 +1,60 @@
+cdrODM<-OD_0_24_SH
 taxiODM<-OD_0_24_SH
-taxiODM[rowSums(taxiODM)!=0]
-taxiODM[colSums(taxiODM)!=0]
 
-#241462
-x<-ODout_SH_3_6[rowSums(ODout_SH_3_6)==0]
-x<-ODout_SH_3_6[colSums(ODout_SH_3_6)==0]
-
-#x ne postoje takvi reci i stupci ->pobriši iz taxi
+  #241462
+  x<-ODout_SH_3_6[rowSums(ODout_SH_3_6)==0]
+  x<-ODout_SH_3_6[colSums(ODout_SH_3_6)==0]
+  #x ne postoje takvi reci i stupci ->pobriši iz taxi
 
 #455625
-x<-taxiODM[rowSums(taxiODM)!=0 & colSums(taxiODM)!=0]
-
+x<-subset(taxiODM, rowSums(taxiODM)!=0 & colSums(taxiODM)!=0)
 #280125
 
 #1188100
-y<-OD_0_24_SH[rowSums(OD_0_24_SH)!=0 & colSums(OD_0_24_SH)!=0]
-#576610
+y<-subset(cdrODM, rowSums(taxiODM)!=0 & colSums(taxiODM)!=0)
+#655090
+
+d_x_1<-data.frame(dimnames(x)[1])
+d_y_1<-data.frame(dimnames(y)[1])
+names(d_x_1)<-c("Lon_Lat")
+names(d_y_1)<-c("Lon_Lat")
+
 
 library(dplyr)
-intersection = intersect(LonLat_CDR, LonLat_SC)
-intersection = intersect(x,y)
+d_1<-intersect(d_x_1,d_y_1)#268
+
+d_x_2<-data.frame(dimnames(x)[2])
+d_y_2<-data.frame(dimnames(y)[2])
+names(d_x_2)<-c("Lon_Lat")
+names(d_y_2)<-c("Lon_Lat")
+
+d_2<-intersect(d_x_2,d_y_2)#675
+
+d_1<-list(d_1)
+d_2<-list(d_2)
+
+x<-x[unlist(d_1),unlist(d_2)]
+
+y<-y[unlist(d_1),unlist(d_2)]
+
+
+heatmap(x, col = heat.colors(256),Rowv=NA,Colv=NA)
+heatmap(y, col = heat.colors(256), Rowv=NA,Colv=NA)
+
+SSIM(x,y,500) # 0.8330692
+
+SSIM(x,y,256) # 0.8326957
+SSIM(y,x,256) # 0.8326957
+
+
+SSIM(x,y,5) # 0.8751913
+
+MSE(x,y) # 1.84314
+MSE(y,x) # 1.84314
+
+# R2_Score not the same!!
+R2_Score(x,y) #  -90.31154
+R2_Score(y,x) #  -0.009956078
+
+
+

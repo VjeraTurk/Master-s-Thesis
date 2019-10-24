1fee7923186c8d19a07f320294cac2d872c79c43
VjeraTurk <vjera.turk@gmail.com>
2019-10-25 01:20:34 +0200

    25.10. ispravljeni grafikoni
    Former-commit-id: 4f997f08efb966e04658b9ed6be2cccdbcdcf633

--- source/kc.R
+++ source/kc.R
@@ -1,5 +1,11 @@
 "Koprivnica osm and BS"
-require("data.table")
+require(sp)
+require(rgdal)
+require(deldir)
+require(dplyr)
+require(ggplot2)
+require(ggthemes)
+
 setwd("/home/adminuser/CODM/masters-thesis/data/cell")
 file="219.csv"
 base_stations<-fread(file=file, sep="auto", header=TRUE, quote="")
@@ -12,36 +18,9 @@ kc_bs<-base_stations[lat < 46.2026 & lat > 46.1232 & lon < 16.9392 & lon > 16.72
 LonLat<-data.frame(kc_bs$lon,kc_bs$lat)
 colnames(LonLat)<-c("Longitude","Latitude")
 #LonLat<-LonLat[1:262,]
-SPointsDF_to_voronoi_SPolysDF <- function(sp) {
-  
-  # tile.list extracts the polygon data from the deldir computation
-  vor_desc <- tile.list(deldir(sp@coords[,1], sp@coords[,2])) #only 262/276?!
-  
-  lapply(1:(length(vor_desc)), function(i) {
-    
-    # tile.list gets us the points for the polygons but we
-    # still have to close them, hence the need for the rbind
-    tmp <- cbind(vor_desc[[i]]$x, vor_desc[[i]]$y)
-    tmp <- rbind(tmp, tmp[1,])
-    
-    # now we can make the Polygon(s)
-    Polygons(list(Polygon(tmp)), ID=i)
-    
-  }) -> vor_polygons
-  
-  # hopefully the caller passed in good metadata!
-  sp_dat <- sp@data
-  
-  # this way the IDs _should_ match up w/the data & voronoi polys
-  rownames(sp_dat) <- sapply(slot(SpatialPolygons(vor_polygons),'polygons'),slot, 'ID')
-  
-  SpatialPolygonsDataFrame(SpatialPolygons(vor_polygons), data=sp_dat)
-  
-}
 
 vor_pts <- SpatialPointsDataFrame(cbind(LonLat$Longitude, LonLat$Latitude), LonLat, match.ID=TRUE)
 vor <- SPointsDF_to_voronoi_SPolysDF(vor_pts)
-sp<-vor_pts
 system.time(vor_df <- fortify(vor))
 
 
@@ -64,14 +43,18 @@ map.latlon <- openproj(map, projection = "+proj=longlat +ellps=WGS84 +datum=WGS8
 gg <- ggplot()
 gg <- autoplot(map.latlon) + geom_point(data=LonLat, aes(x=Longitude, y=Latitude),size=1, shape=21, color="white", fill="steelblue")+ geom_map(data=vor_df, map=vor_df, aes(x=long, y=lat, map_id=id), color="#a5a5a5", fill="#FFFFFF00", size=0.25)
 
+#### Agregacija ćelija
 require("Matrix.utils")
-#agg<-aggregate.Matrix(m, fun="sum")
+#agg<-aggregate.Matrix(m, fun="sum") # moguce ok pristup
 
-x<-matrix(sample.int(15, 33*33, TRUE), 33, 33)
+#loš pristup - sumiranje blokova elemenata - samo za probu
+#https://stackoverflow.com/questions/16884384/blockwise-sum-of-matrix-elements
+
+x<-matrix(sample.int(15, 33*33, TRUE), 33, 33) # random matrica
 diag(x) <- 0
 heatmap(x,Rowv = NA,Colv = NA)
 
-y<-matrix(sample.int(15, 33*33, TRUE), 33, 33)
+y<-matrix(sample.int(15, 33*33, TRUE), 33, 33) # random matrica
 diag(y) <- 0
 heatmap(y,Rowv = NA,Colv = NA)
 
@@ -87,7 +70,6 @@ length(x[x==0]) # 1093177
 y<-ztaxi # treba taxi izvrtiti do kraja, nije dovoljno 1m ?!
 length(y[y==0]) # 1182918
 
-#https://stackoverflow.com/questions/16884384/blockwise-sum-of-matrix-elements
 b <- mat(1090,10)
 b
 x_10<-t(b) %*% x %*% b
@@ -106,6 +88,7 @@ mod1<-lm(y~x)
 summary(mod1)
 abline(mod1, lwd=2, col="red")
 
+SSIM(y,x,5) # 0.998
 SSIM(y_10,x_10,5) #[1] 0.861207
 
 "Call:
@@ -154,3 +137,11 @@ Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’
 Residual standard error: 14.06 on 11879 degrees of freedom
 Multiple R-squared:  0.3671,	Adjusted R-squared:  0.3671 
 F-statistic:  6892 on 1 and 11879 DF,  p-value: < 2.2e-16"
+
+
+
+## complete linckage?! #max
+## single linkage #min
+## average linkage
+
+kmeans(kmeans())

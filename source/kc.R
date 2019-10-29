e53e42dd3918955b090ac52bb9e706483edc5790
VjeraTurk <vjera.turk@gmail.com>
2019-10-29 04:35:34 +0100

    Zg,Vž,Kc
    Former-commit-id: 76660b2327c07d7557f15935e32498a544419f1e

--- source/kc.R
+++ source/kc.R
@@ -1,29 +1,74 @@
-"Koprivnica osm and BS"
+"Koprivnica, Zagreb osm and BS"
 require(sp)
 require(rgdal)
 require(deldir)
 require(dplyr)
 require(ggplot2)
 require(ggthemes)
+require(data.table)
 
 setwd("/home/adminuser/CODM/masters-thesis/data/cell")
 file="219.csv"
 base_stations<-fread(file=file, sep="auto", header=TRUE, quote="")
 "
-        46.2026
-16.7246         16.9392
-        46.1232
+        46.22
+16.73 Koprivnica  16.95
+        46.10
+
+        45.8410
+15.9007  Zagreb  16.0540
+        45.7523
+        46.35
+16.27 Varaždin  16.39
+        46.26
+
 "
-kc_bs<-base_stations[lat < 46.2026 & lat > 46.1232 & lon < 16.9392 & lon > 16.7246 & radio=="LTE"] #276 obs. #74 GSM #47 LTE
-LonLat<-data.frame(kc_bs$lon,kc_bs$lat)
+#bs_subset<-base_stations[lat < 46.22 & lat > 46.10 & lon < 16.95 & lon > 16.73 & radio=="GSM"] #276 obs. #74 GSM #47 LTE
+#bs_subset<-base_stations[lat < 45.86 & lat > 45.7523 & lon < 16.08 & lon >  15.87 & radio=="GSM"] #bez GSM ih bude previše
+bs_subset<-base_stations[lat < 46.35 & lat > 46.26 & lon < 16.39 & lon > 16.27 & radio=="GSM"] #276 obs. #74 GSM #47 LTE
+
+#11730 GSM ->3115
+LonLat<-data.frame(bs_subset$lon,bs_subset$lat)
 colnames(LonLat)<-c("Longitude","Latitude")
-#LonLat<-LonLat[1:262,]
+
+SPointsDF_to_voronoi_SPolysDF <- function(sp) {
+  
+  # tile.list extracts the polygon data from the deldir computation
+  vor_desc <- tile.list(deldir(sp@coords[,1], sp@coords[,2]))
+
+  lapply(1:(length(vor_desc)), function(i) {
+    
+    # tile.list gets us the points for the polygons but we
+    # still have to close them, hence the need for the rbind
+    tmp <- cbind(vor_desc[[i]]$x, vor_desc[[i]]$y)
+    tmp <- rbind(tmp, tmp[1,])
+    
+    # now we can make the Polygon(s)
+    Polygons(list(Polygon(tmp)), ID=i)
+    
+  }) -> vor_polygons
+  
+  # hopefully the caller passed in good metadata!
+  #sp_dat <- sp@data #ne radi
+  sp_dat<-data.frame()
+  for (v in vor_desc) {
+    sp_dat<-rbind(sp_dat,v$pt)
+  }
+  names(sp_dat)<-c("Longitude", "Latitude")
+  
+  # this way the IDs _should_ match up w/the data & voronoi polys
+  rownames(sp_dat) <- sapply(slot(SpatialPolygons(vor_polygons),'polygons'),slot, 'ID')
+  
+  SpatialPolygonsDataFrame(SpatialPolygons(vor_polygons),
+                           data=sp_dat)
+  
+}
 
 vor_pts <- SpatialPointsDataFrame(cbind(LonLat$Longitude, LonLat$Latitude), LonLat, match.ID=TRUE)
 vor <- SPointsDF_to_voronoi_SPolysDF(vor_pts)
 system.time(vor_df <- fortify(vor))
 
-
+############################
 library(OpenStreetMap)
 LAT1 = 46.1232
 LON1 = 16.7246
@@ -42,6 +87,7 @@ map <- openmap(c(LAT2,LON1), c(LAT1,LON2), zoom = NULL,
 map.latlon <- openproj(map, projection = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 gg <- ggplot()
 gg <- autoplot(map.latlon) + geom_point(data=LonLat, aes(x=Longitude, y=Latitude),size=1, shape=21, color="white", fill="steelblue")+ geom_map(data=vor_df, map=vor_df, aes(x=long, y=lat, map_id=id), color="#a5a5a5", fill="#FFFFFF00", size=0.25)
+
 
 #### Agregacija ćelija
 require("Matrix.utils")

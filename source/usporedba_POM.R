1ebef2e957130667c8801390e1bb020e8b913e7f
VjeraTurk <vjera.turk@gmail.com>
2019-12-05 19:39:26 +0100

    IN PROGRESS 6
    Former-commit-id: 4fdb776348d5e294438512cf0f81d8dfb7fec511

--- source/usporedba_POM.R
+++ source/usporedba_POM.R
@@ -49,26 +49,61 @@ length(B_0_24[B_0_24>0])/length(B_0_24[B_0_24==0]) #  0.08683223 +
     length(B[B>0])/(length(B[B==0]) + length(B[B < k*m]))  #  0.008014788 | k=2 +
 
 ran = c(A_0_24) 
-col_breaks = seq (0, max(ran),length = 5)
-colPal = colorRampPalette(c('red'))(4)
-heatmap(A_0_24, Colv = NA, Rowv = NA, main = "A_0_24", breaks = col_breaks, col = colPal)
+#col_breaks = seq (0, max(ran),length = 5)
+#colPal = colorRampPalette(c('red'))(4)
+#colPal = gray((1:4)/6)
+#colPal = rgb((1:4)/4, 0, 0)
+#colPal = rainbow(4, start = 0, end = 0.25)
+#heatmap(A_0_24, Colv = NA, Rowv = NA, main = "A_0_24", breaks = col_breaks, col = colPal)
 
-ran = c(B_0_24) 
-col_breaks = seq (0, max(ran),length = 5)
-colPal = colorRampPalette(c('red'))(4)
-heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col = colPal)
+#ran = c(B_0_24) 
+#col_breaks = seq (0, max(ran),length = 5)
+#colPal = colorRampPalette(c('red'))(4) -> sve ista boja, crvena!
+#heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col = colPal)
 
-#my.breaks <-c(0,1,5,10,20,30,40,50,100)
-my.breaks <-c(0,1,5,10,20,30,40,50,100)
-#my.col=c("white","red","green")
+  #my.breaks <-c(0,1,5,10,20,30,40,50,100)
+  #my.breaks <-c(0,1,5,10,20,30,40,50,100)
+  #my.col=c("white","red","green")
 
-require(unikn)
-col_breaks = seq (0, max(ran),length = 12)
-col_breaks <-c(0,1,2,5,10,15,20,30,40,50,60)
-heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col = usecol(pal_unikn, n = 10))
-#https://cran.r-project.org/web/packages/unikn/vignettes/colors.html
+  #https://cran.r-project.org/web/packages/unikn/vignettes/colors.html
+  #require(unikn)
+  #colPal  = usecol(pal_unikn, n = 10)
+  #col_breaks = seq (0, max(ran),length = 12)
 
-require(lattice)
-levelplot(A_0_24)
+col_breaks <-c(0,1,2,5,10,15,20,30,40,50,max(A_0_24))
+colPal = rainbow(10, start = 0, end = 0.25)
+heatmap(A_0_24, Rowv=NA, Colv=NA, main = "A_0_24", breaks =col_breaks, col = colPal, scale="none", margins=c(10,10))
+heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col =colPal, scale="none", margins=c(10,10))
 
-heatmap(A_0_24, Rowv=NA, Colv=NA, scale="none", margins=c(10,10))
+heatmap(A_0_24, Rowv=NA, Colv=NA, main = "A_0_24", breaks =col_breaks, col = hcl.colors(10, "YlOrRd", rev = TRUE), scale="none", margins=c(10,10))
+heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col =hcl.colors(10, "YlOrRd", rev = TRUE), scale="none", margins=c(10,10))
+#RdYlBu
+#YlOrRd
+
+#require("apcluster")
+#heatmap(A_0_24, Rowv=NA, Colv=NA, main = "A_0_24", breaks =col_breaks, col = hcl.colors(10, "YlOrRd", rev = TRUE), scale="none", margins=c(10,10))
+#heatmap(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col =hcl.colors(10, "YlOrRd", rev = TRUE), legend="col", scale="none", margins=c(10,10))
+
+require(gplots)
+col_breaks = seq (1, max(ran),length = 100)
+
+col_breaks <-c(0,1,2,5,10,15,20,30,40,50,100,max(A_0_24))
+col = c("#000000",hcl.colors(10, "RdYlBu", rev = TRUE))
+
+heatmap.2(A_0_24, Rowv=NA, Colv=NA, dendrogram = "none", scale="none", 
+          breaks =col_breaks, col = col, density.info="none", trace="none",  
+          labRow = FALSE, labCol = FALSE, key=TRUE, key.title=NULL, 
+          lmat = rbind(c(2,4),c(3,1)), lhei = c(0.75,3), lwid = c(2,10), key.xlab="širina toka")
+
+#col_breaks <-c(0,5,25,50,100,250,500,750,1000,1500,2000,3716) #3716 je 4. najveći rezultat
+#col_breaks od A :|
+col = c("#000000",hcl.colors(10, "RdYlBu", rev = TRUE))
+
+heatmap.2(B_0_24, Rowv=NA, Colv=NA, dendrogram = "none", scale="none", 
+          breaks =col_breaks, col = col, density.info="none", trace="none",  
+          labRow = FALSE, labCol = FALSE, key=TRUE, key.title=NULL, 
+          lmat = rbind(c(2,4),c(3,1)), lhei = c(0.75,3), lwid = c(2,10), key.xlab="širina toka")
+
+
+#heatmap.2(B_0_24, Colv = NA, Rowv = NA, main = "B_0_24", breaks = col_breaks, col =hcl.colors(10, "YlOrRd", rev = TRUE), scale="none", margins=c(10,10), dendrogram = "none", key=TRUE)
+
